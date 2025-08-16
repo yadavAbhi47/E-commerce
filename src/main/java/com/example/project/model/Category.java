7d21fc717f77b2d1473b67421525ae6c486b30aa
Abhi <abhi.yadavh2o@gmail.com>
2025-08-16 22:10:39 +0530

    E-commerce project

--- src/main/java/com/example/project/model/Category.java
+++ src/main/java/com/example/project/model/Category.java
@@ -1,11 +1,14 @@
 package com.example.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
