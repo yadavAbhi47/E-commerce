3c6993a00d167d922d342d2b378d2c4d3a84fcd5
Abhishek Yadav <abhi.yadavh2o@gmail.com>
2026-02-28 20:20:42 +0530

    check connection

--- src/main/java/com/example/project/config/AppConfig.java
+++ src/main/java/com/example/project/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class AppConfig {
     @Bean
